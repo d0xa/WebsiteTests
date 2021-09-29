4cefdacb894025d13d09ceda4a44240f334c57ab
d0xa <raguil12043@gmail.com>
2021-09-28 18:07:08 -0600

    updated url networks

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,6 @@
-#from django.urls import path
 from django.urls import path
 from .views import RoomView
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('home',RoomView.as_view()),
+    path('room', RoomView.as_view()),
 ]
-
---
+++ frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'React';
+
+export default class RoomJoinPage extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <p>This is the room join page</p>
+    }
+}
